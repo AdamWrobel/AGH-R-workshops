6be6a8adaa51b9e65b17fcd44d9d0b2f442fb7c5
Adam Wróbel <wrobel_adam@o2.pl>
2018-03-27 18:34:03 +0200

    Add files via upload

--- 4_data_processing_and_modelling/multivariate_regression.R
+++ 4_data_processing_and_modelling/multivariate_regression.R
@@ -1,4 +1,7 @@
 ## Valuation of a property - multivariate linear model ##
+library(dplyr)
+library(ggplot2)
+options(scipen = 10)
 
 adress <- 'https://raw.githubusercontent.com/AdamWrobel/AGH-R-workshops/master/4_data_processing_and_modelling/house_prices.csv'
 house_prices <- read.csv(adress, stringsAsFactors = F) %>% data.frame
@@ -12,9 +15,9 @@ str(house_prices)
 head(house_prices,2)
 summary(house_prices)
 
-# Exercise 2
 
-# initial data processing
+
+## initial data processing ##
 
 # variables with mostly NAs
 logicalNAs <- is.na(house_prices) 
@@ -26,36 +29,112 @@ house_prices2 <- house_prices %>% select(variables_to_keep)
 
 
 # drop variables with same value for almost all observations
-categorigal_variables <- apply(house_prices2, 2, is.character)
+categorigal_variables <- sapply(house_prices2, is.character)
+subset_with_cat_var <- house_prices2[,categorigal_variables]
+
+subset_with_cat_var %>% sapply(table)
+
+house_prices3 <- house_prices2 %>% 
+  select(-Street,-Utilities,-LandSlope,-Condition1,Condition2,-RoofMatl,-BsmtCond,
+         -Heating,-Electrical,-Functional,-GarageQual,-GarageCond,-PavedDrive)
 
 
-# select 
-house_prices_small <- house_prices %>% 
-  select(-PoolQC,-Fence,-MiscFeature,-FireplaceQu,-Alley) %>% # drop variables with mostly NAs
-  select(-Exterior2nd, -Exterior1st, -Neighborhood) %>% # drop with many levels
-  select(-RoofMatl,-Condition2,-Condition1,-LandSlope,-Utilities,-Street,-BsmtCond,-Heating,-Electrical,-Functional,-GarageQual,-GarageCond,-PavedDrive) %>% # drop not informative
-  na.omit  # drop observation with NAs
+# drop variables with to many levels
+house_prices4 <- house_prices3 %>% 
+  select(-Neighborhood,-Exterior1st,-Exterior2nd,-MSSubClass)
 
-fctr <- house_prices_small[sapply(house_prices_small, is.character)]
-sapply(fctr, table)
+# drop observations with missing value and drop id variable
+house_prices5 <- house_prices4 %>% na.omit %>% select(-Id)
 
 
-house_prices_small <- house_prices %>% 
-  select(SalePrice,OverallQual,GrLivArea, GarageCars, TotalBsmtSF, FullBath, TotRmsAbvGrd)
+## investigate variables ##
+
+# numeric variables
+numeric_variables <- sapply(house_prices5, is.numeric)
+subset_with_num_var <- house_prices5[,numeric_variables]
+dim(subset_with_num_var)
+correlation <- cor(subset_with_num_var)[,35]
+
+corelated <- correlation[abs(correlation) > 0.25]
+
+# categorical variables
+categorigal_variables <- sapply(house_prices5, is.character) 
+categorigal_variables[length(categorigal_variables)] <- T
+subset_with_cat_var <- house_prices5[,c(categorigal_variables)]
+
+subset_with_cat_var %>% group_by(MSZoning) %>% summarize(mean_price = mean(SalePrice))
+subset_with_cat_var %>% group_by(CentralAir) %>% summarize(mean_price = mean(SalePrice))
+
+subset_with_cat_var %>% ggplot(aes(SalePrice, fill = CentralAir)) + geom_density(alpha = 0.5)
+
+# exercise 2
+# a) calculate standard deviation for each numerical variable using
+# apply function and subset_with_num_var object
 
 
-initial_model <- lm(SalePrice~.,house_prices_small)
+# b) calculate mean LotArea using dplyr functions
+
+
+# c) calculate mean LotArea for each level of MSZoning variable (using dplyr functions)
+
+
+
+# specify the model with one categorical variable
+one_factor_model <- lm(SalePrice~CentralAir,house_prices5)
+summary(one_factor_model)
+
+# add one more categorical variable
+two_factor_model <- lm(SalePrice~CentralAir + MSZoning,house_prices5)
+summary(two_factor_model)
+
+
+# model with all the numeric variable with correlation with SalePrice higher than 0.25
+cor_var <- corelated[-20] %>% names
+Formula <- as.formula(paste("SalePrice ~ ", gsub(", "," + ",toString(cor_var))))
+numeric_variables_model <- lm(Formula,house_prices5)
+summary(numeric_variables_model)
+
+
+# specify the model with all variable
+initial_model <- lm(SalePrice~.,house_prices5)
 summary(initial_model)
 
-model_stepwise <- step(initial_model)
+
+# apply a stepwise optimization algorithm
+model_stepwise <- step(initial_model, direction = 'both')
 summary(model_stepwise)
 
-initial_model <- lm(SalePrice~OverallQual+GrLivArea+GarageCars+TotalBsmtSF+FullBath+TotRmsAbvGrd,house_prices)
-summary(initial_model)
-step(initial_model, direction = 'both')
 
-initial_model
+# plot the fitted vs observed price
+fitted <- model_stepwise %>% fitted
+observed <- house_prices5$SalePrice
+plot(fitted,observed, pch = 1, col = 'firebrick1', xlim = c(0,5*10^5), ylim = c(0,5*10^5))
+grid()
 
-MSSubClass
+# plot the fitted vs residuals 
+residuals_step <- model_stepwise %>% residuals
+plot(fitted,residuals_step, pch = 1, col = 'cadetblue3')
+grid()
 
-house_prices %>% lapply(table)
+
+# Exercise 3 
+# a) plot the fitted vs observed price for numeric_variables_model
+
+
+
+# plot the fitted vs residuals for numeric_variables_model
+
+
+
+
+
+
+
+# other elements that could be checked/improved
+plot(model_stepwise)
+
+# outliers
+
+# combining some levels in the categorical variables
+
+# droping some variables to get simplier model
